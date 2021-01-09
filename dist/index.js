570f2b36f0b0cf1c2fe3a8dbe6c53127c8e3750e
elimerl <32183908+elimerl@users.noreply.github.com>
2021-01-09 06:12:26 +0000

    Deploying to gh-pages from  @ a56ed523bd122bcfe420c92ecd645715d0ed544d 🚀

--- dist/index.js
+++ dist/index.js
@@ -2,14 +2,16 @@ import {mathquillToMathJS} from "./preprocessMathQuill.js";
 import {compile} from "../web_modules/mathjs.js";
 import "../web_modules/toastify-js/src/toastify.css.proxy.js";
 const equationSpan = document.getElementById("equation");
-console.log(equationSpan);
+let compiled;
+let compiledSrc;
 const MQ = MathQuill.getInterface(2);
 const field = MQ.MathField(equationSpan);
 const canvas = document.getElementById("chart");
 const ctx = canvas.getContext("2d");
 const startBtn = document.getElementById("start");
-let startTime;
+let startTime = Date.now();
 startBtn.onclick = () => {
+  document.getElementById("time").innerText = "0";
   const equation = mathquillToMathJS(field.latex());
   try {
     startTime = Date.now();
@@ -19,23 +21,37 @@ startBtn.onclick = () => {
   }
 };
 setInterval(() => {
+  if (!mathquillToMathJS(field.latex()).includes("t"))
+    return;
   try {
     runEquation(mathquillToMathJS(field.latex()));
+    document.getElementById("time").innerText = ((Date.now() - startTime) / 1e3).toFixed(1);
   } catch (error) {
   }
 }, 10);
+runEquation(mathquillToMathJS(field.latex()));
 function mod(n, m) {
   return (n % m + m) % m;
 }
 function runEquation(equation) {
-  const compiled = compile(equation);
+  if (compiledSrc !== equation) {
+    console.log("recompiling equation");
+    compiled = compile(equation);
+    compiledSrc = equation;
+  }
   const answers = [];
   for (let i = 0; i < canvas.height; i++) {
-    answers.push(compiled.evaluate({x: i, t: Date.now() - startTime}));
+    answers.push(compiled.evaluate({x: i, t: (Date.now() - startTime) / 1e3}));
   }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "black";
-  answers.forEach((ans, i) => {
-    ctx?.fillRect(i, canvas.width - mod(ans, canvas.width), 3, 3);
+  ctx.strokeStyle = "black";
+  ctx?.beginPath();
+  ctx?.moveTo(0, canvas.width - mod(answers[0], canvas.width));
+  answers.forEach((ans, x) => {
+    const y = canvas.width - mod(ans, canvas.width);
+    ctx?.lineTo(x, y);
+    ctx?.stroke();
+    ctx?.beginPath();
+    ctx?.moveTo(x, y);
   });
 }
